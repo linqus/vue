0876f65df11e145b4ba50f2c6af5e9038514f39e
Karol Linkiewicz <karol@linkiewicz.pl>
2023-09-24 21:15:34 +0200

    fetchProducts as promise

---
+++ assets/js/services/products-service.js
@@ -0,0 +1,17 @@
+import axios from 'axios';
+
+/**
+ * @param {string|null} categoryIri
+ * @returns {Promise}
+ */
+export function fetchProducts(categoryIri = null) {
+    const params = {};
+
+    if (categoryIri) {
+        params.category = categoryIri;
+    }
+
+    return axios.get('/api/products', {
+        params,
+    });
+}
