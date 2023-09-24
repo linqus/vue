587b2681af2e8d22a615f982642dd327076b8cc7
Karol Linkiewicz <karol@linkiewicz.pl>
2023-09-24 21:03:48 +0200

    categories-service for fetching categories from api & using it

--- assets/js/components/sidebar.vue
+++ assets/js/components/sidebar.vue
@@ -41,7 +41,7 @@
 </template>
 
 <script>
-import axios from 'axios';
+import { fetchCategories } from '@/services/categories-service';
 import Loading from '@/components/loading';
 
 export default {
@@ -70,8 +70,7 @@ export default {
         },
     },
     async created() {
-        const response = await axios.get('/api/categories');
-
+        const response = await fetchCategories();
         this.categories = response.data['hydra:member'];
     },
 };
