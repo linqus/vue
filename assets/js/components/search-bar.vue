8a2a3828403178af08550d3c0093cca5ba1a98b8
Karol Linkiewicz <karol@linkiewicz.pl>
2023-09-25 15:34:51 +0200

    searchbar

---
+++ assets/js/components/search-bar.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+        <input
+            :value="searchTerm"
+            type="search"
+            class="form-control"
+            placeholder="Search Product..."
+            @input="searchTerm = $event.target.value"
+        >
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'SearchBar',
+    data() {
+        return {
+            searchTerm: '',
+        };
+    },
+};
+
+</script>
